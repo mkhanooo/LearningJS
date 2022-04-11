3410860e6358bfef6b6955cc8d51a1699e94f6b2
mkhanooo <mkhan2khan@live.com>
2022-04-11 18:15:15 +0500

    day 4 complete

---
+++ Day4/arrayMethods.js
@@ -0,0 +1,19 @@
+// let array = [10,20,30,40,50];
+// array.unshift(0);
+// console.log(array);
+// array.push(60);
+// console.log(array);
+// array.shift();
+// console.log(array);
+
+let array = [10,20,30,40,50];
+// let index = array.indexOf(30);
+// console.log(index);
+
+// console.log(array.reverse());
+
+// slice method
+// console.log(array.slice(0,2));
+// splice method
+// console.log(array.splice(0,2));
+// console.log(array)
